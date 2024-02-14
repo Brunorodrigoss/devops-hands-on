d04e2c5d83295392cf4cc6227c0d78ee008ad374
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-02-14 10:49:52 +0000

    Vagrant - Creating the first virtual machine

--- vagrant-lab/provision.sh
+++ vagrant-lab/provision.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-echo "Installing Apache and setting it up ..."
-yum update httpd
+
+echo "Installing Apache and setting it up..."
+
 yum install -y httpd >/dev/null 2>&1
 cp -r /vagrant/html/* /var/www/html/
-systemctl restart httpd
+service httpd start
